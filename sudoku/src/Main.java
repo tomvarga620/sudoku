6587b38c808df1239b6e92b7f52883ba287a3abf
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-10-09 08:08:47 +0200

    added methods,solved sudoku

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -5,15 +5,17 @@ public class Main {
 
         String input = "100026039008700601000000200400900516000000000796004008009000000301002900640180003";
 
+        //toto tu
         int[][] numbers = Arrays.stream(input.split("(?<=\\G.{9})"))
         .map(s -> (Arrays.stream(s.split("(?<=\\G.{1})")).mapToInt(Integer::parseInt)
         .toArray())).toArray(int[][]::new);
 
-        for(int i=0;i<=9;i++){
-            for(int j=0;j<=9;j++){
-
+        /*for(int i=0;i<9;i++){
+            System.out.println(" ");
+            for(int j=0;j<9;j++){
+                System.out.print(" "+numbers[i][j]);
             }
-        }
+        }*/
 
             int[][] testboard = {
                     {8, 0, 3, 7, 5, 0, 0, 0, 2},
@@ -27,7 +29,17 @@ public class Main {
                     {7, 0, 0, 0, 2, 4, 5, 0, 6},
             };
 
-            Sudoku sudoku = new Sudoku();
-            //sudoku.generateBoard(testboard);
+            Sudoku sudoku = new Sudoku(numbers);
+            sudoku.board();
+
+            sudoku.printResutl();
+           if(sudoku.solveSudoku()){
+                System.out.println("good");
+                sudoku.printResutl();
+
+            }else{
+                System.out.println("bad");
+                sudoku.printResutl();
+            }
         }
     }
